451192cab4231f73bae7a4edd811c06a25ff2c74
Tabassum-Wasila <twahsila@gmail.com>
2024-07-24 02:30:36 +0800

    Basic shell script for automating new css project folder structure

---
+++ starter.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 Please supply a folder name"
+    exit 1
+fi
+
+mkdir "$1"
+
+cd "$1"
+
+touch "index.html"
+
+mkdir "css"
+
+touch "css/style.css"
+
+if [ -z "$2" ]; then
+    echo "Usage: Unable to find Document title."
+    read -p "Title: " TITLE
+else
+    TITLE=$2
+fi
+
+cat << EOL > "index.html"
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>$TITLE</title>
+</head>
+<body>
+    
+</body>
+</html>
+EOL
+
+echo "Successfully created folder $1 with Document Title: $TITLE"

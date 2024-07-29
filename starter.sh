83eedbc595de18f0a75b9d0b2a85625321209ca3
Tabassum-Wasila <twahsila@gmail.com>
2024-07-29 19:49:15 +0800

    Created box animations

--- starter.sh
+++ starter.sh
@@ -29,6 +29,7 @@ cat << EOL > "index.html"
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>$TITLE</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     
